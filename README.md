3b2b1a218f38c9a2fca2b300c6300f54e81ee135
LeSaR <ilkov2006@gmail.com>
2025-02-12 18:05:07 -0500

    Added environment variable for port

---
+++ README.md
@@ -0,0 +1,4 @@
+# Serve port
+
+To specify a port to serve on, either set the `SERVE_PORT` environment variable,
+or pass it as the first argument like this: `web-serve 8888`
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::{env::args, net::SocketAddr};
+use std::{env, net::SocketAddr};
 
 use axum::{extract::Path, routing::get, Router};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-	let port: u16 = args().nth(1).and_then(|s| s.parse().ok()).unwrap_or(8080);
+	let port: u16 = env::args()
+		.nth(1)
+		.and_then(|s| s.parse().ok())
+		.or_else(|| env::var("SERVE_PORT").ok()?.parse().ok())
+		.unwrap_or(8080);
 
-	let router = Router::new().route("/{*key}", get(handler));
+	let router = Router::new().route("/{*path}", get(handler));
 	let listener = TcpListener::bind(SocketAddr::from(([0u8, 0, 0, 0], port)))
 		.await
 		.unwrap();
@@ -16,6 +20,6 @@ async fn main() {
 	axum::serve(listener, router).await.unwrap();
 }
 
-async fn handler(Path(path): Path<String>) -> String {
-	path
+async fn handler(Path(path): Path<std::path::PathBuf>) -> String {
+	path.to_str().unwrap_or("").to_owned()
 }
