efc5fc4aa715d80ef729680e1e909018728e1697
LeSaR <ilkov2006@gmail.com>
2025-02-17 17:30:58 -0500

    Added main functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use std::{env, net::SocketAddr};
+use std::{env, fs::read_to_string, net::SocketAddr, path::PathBuf};
 
-use axum::{extract::Path, routing::get, Router};
+use axum::{
+	extract::Path as PathExt,
+	http::{header, StatusCode},
+	response::IntoResponse,
+	routing::get,
+	Router,
+};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -11,7 +17,9 @@ async fn main() {
 		.or_else(|| env::var("SERVE_PORT").ok()?.parse().ok())
 		.unwrap_or(8080);
 
-	let router = Router::new().route("/{*path}", get(handler));
+	let router = Router::new()
+		.route("/{*path}", get(handler))
+		.route("/", get(root));
 	let listener = TcpListener::bind(SocketAddr::from(([0u8, 0, 0, 0], port)))
 		.await
 		.unwrap();
@@ -20,6 +28,33 @@ async fn main() {
 	axum::serve(listener, router).await.unwrap();
 }
 
-async fn handler(Path(path): Path<std::path::PathBuf>) -> String {
-	path.to_str().unwrap_or("").to_owned()
+async fn root() -> &'static str {
+	"Hello from web-serve!"
+}
+
+async fn handler(PathExt(path): PathExt<PathBuf>) -> Result<impl IntoResponse, impl IntoResponse> {
+	match read_to_string(&path) {
+		Ok(file) => Ok((
+			StatusCode::OK,
+			[(
+				header::CONTENT_TYPE,
+				mime_guess::from_path(&path)
+					.first()
+					.map_or_else(|| "application/json".to_owned(), |m| m.to_string()),
+			)],
+			file,
+		)),
+		Err(err) => {
+			eprintln!("Error reading file: {err}");
+			Err(not_found())
+		}
+	}
+}
+
+fn not_found() -> impl IntoResponse {
+	(
+		StatusCode::NOT_FOUND,
+		[(header::CONTENT_TYPE, "text/plain")],
+		StatusCode::NOT_FOUND.to_string(),
+	)
 }
